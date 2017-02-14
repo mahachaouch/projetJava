ad2c14a22618201347537a0eb45cdba56a82bb05
mahachaouch <maha-chaouch@hotmail.fr>
2017-02-14 12:12:48 +0100

    Ajout de vue competences pour tester

--- Projet Java/src/Vue/FenetreGestionMission.java
+++ Projet Java/src/Vue/FenetreGestionMission.java
@@ -2,20 +2,26 @@ package Vue;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
-public class FenetreGestionMission extends JFrame {
+public class FenetreGestionMission extends JFrame implements ActionListener {
 	
-	   private VueGestionMission vueGestionMission;
+	private VueGestionMission vueGestionMission;
+	private VueCompetences vueCompetences;
 	public FenetreGestionMission(){
     this.setTitle("Gestionnaire des Missions");
     this.setLayout(new BorderLayout());
 
-    //Création de l'item "Jeu"
+    //CrÃ©ation de l'item "Jeu"
     JMenu fichier = new JMenu("File");
     JMenuItem rejouer = new JMenuItem("Nouveau");
     fichier.add(rejouer);
@@ -27,21 +33,27 @@ public class FenetreGestionMission extends JFrame {
 
 
 
-    //Création de l'item "Option"
+    //CrÃ©ation de l'item "Option"
     JMenu option = new JMenu("Option");
 
 
-    //Création de la bar de menu
+    //CrÃ©ation de la bar de menu
     JMenuBar jMenuBar = new JMenuBar();
     jMenuBar.add(fichier);
     jMenuBar.add(option);
-    //TODO AJOUTER LISTENER
+    
 
-    //Ajout de la bar de menu et de la vue à la fenetre
+    //Ajout de la bar de menu et de la vue Ã  la fenetre
     this.add(jMenuBar,BorderLayout.NORTH);
     this.vueGestionMission=new VueGestionMission();
     this.add(this.vueGestionMission,BorderLayout.CENTER);
     
+    //Ajout de la vue Competences
+     JButton b = new JButton("Afficher liste compétences");
+     b.addActionListener((ActionListener) this);
+     
+    this.vueCompetences=new VueCompetences();
+    this.add(this.vueCompetences, BorderLayout.LINE_END);
 
     this.pack();
     this.setVisible(true);
@@ -49,6 +61,20 @@ public class FenetreGestionMission extends JFrame {
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	
 }
+	
+	
+	 public void actionPerformed(ActionEvent e) {
+	        JFrame frame2 = new JFrame("Accueil");
+	        frame2.setVisible(true);
+	        frame2.setSize(600, 600);
+	        JLabel label = new JLabel("test");
+	        JPanel panel = new JPanel();
+	        frame2.add(panel);
+	        panel.add(label);
+	    }
+	
+	
+	
 public static void main(String[] args) {
     FenetreGestionMission fenetreGestionMission=new  FenetreGestionMission();
     fenetreGestionMission.setSize(600,450);

3a884fd713c8be883d0439f89af1b11feabb6ac2
mahachaouch <maha-chaouch@hotmail.fr>
2017-02-26 19:49:56 +0100

    ajout de fctions
    fct ajouter ligne dans un csv
    fct modifier une ligne dans un csv

--- Projet Java/src/Modele/CSV.java
+++ Projet Java/src/Modele/CSV.java
@@ -3,8 +3,13 @@ package Modele;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class CSV {
 
@@ -78,4 +83,85 @@ public class CSV {
 		}
 		return ligne;
 	}
+	
+	//ajoutre une ligne dans un csv
+	// line doit etre sous la forme : "aa;bb;cc" ceci va etre géré par l appelant
+	//file : fichier dans lequel on souhaite ajouter une ligne
+		public void addRawCsv(File file,String line) throws IOException{
+			
+			String seperator = ";" ;
+			
+			String fileNAme= file.getName();
+			String Directory = System.getProperty("user.dir");
+			Directory+="\\src\\Bd\\"+fileNAme ;
+			
+			//initialise PrintWriter object
+			java.io.FileWriter outfile = null;
+			
+			try {
+				 //create PrintWriter object on new File object
+				outfile = new java.io.FileWriter(Directory, true);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} //true = append
+			
+			//adapt line to csv format(dans le cas ou on recoit un String "aa bb bb"
+			//line=line.replaceAll(" ", ";");
+			
+			//write the line
+			outfile.write(line + "\n");
+
+		    outfile.close();
+		}
+		
+		
+		//met à jour une case du cvs (croisement entre ligne et colone)
+		
+		public static void updateCSV(String fileToUpdate, String replace,
+			    int row, int col) throws IOException {
+
+			File inputFile = new File(fileToUpdate);
+
+			// Read existing file 
+			CSVReader reader = new CSVReader(new FileReader(inputFile), ';');
+			ArrayList<String[]> csvBody = (ArrayList)reader.readAll();
+			// get CSV row column  and replace with by using row and column
+			csvBody.get(row)[col] = replace;
+			reader.close();
+
+			// Write to CSV file which is open
+			CSVWriter writer = new CSVWriter(new FileWriter(inputFile), ';');
+			writer.writeAll(csvBody);
+			writer.flush();
+			writer.close();
+			}
+		
+		
+		public static void test(String s,String line) throws IOException{
+			java.io.FileWriter outfile = null;
+			try {
+				outfile = new java.io.FileWriter(s, true);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} //true = append
+		    outfile.write(line.replaceAll(" ", ";") + "\n");
+
+		    outfile.close();
+		
+		}
+		
+		
+		public static void main(String[] args) throws IOException{
+			String Directory = System.getProperty("user.dir");
+			Directory+="\\src\\Bd\\liste_competences.csv";
+			test(Directory,"A4 JAVA2 SWING2 29");
+			
+			//updateCSV(Directory,"xxx",2,2);
+			
+			//String s1="hello there bla bla2";
+			
+		}
 }

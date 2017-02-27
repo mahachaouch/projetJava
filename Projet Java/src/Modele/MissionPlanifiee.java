3d8732671872554c6cea3107db4bfab68442ef6d
mahachaouch <maha-chaouch@hotmail.fr>
2017-02-28 00:46:24 +0100

    ajout de nouvelle fctions

--- Projet Java/src/Modele/MissionPlanifiee.java
+++ Projet Java/src/Modele/MissionPlanifiee.java
@@ -3,7 +3,10 @@ package Modele;
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.Map.Entry;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class MissionPlanifiee extends MissionEnPreparation{
 
@@ -13,7 +16,7 @@ public class MissionPlanifiee extends MissionEnPreparation{
 		 this.ListPersonneParCometence = new Hashtable<Competence,ArrayList<Personne>>();
 	 }
 	 
-	 //ajoute une personne � la liste des personnes ayant la comp�tence donn�e
+	 //ajoute une personne à la liste des personnes ayant la compétence donnée
 	 public void affecterPersonne(Personne p,Competence Compet){
 		 
 		 if(this.ListPersonneParCometence.containsKey(Compet)) {
@@ -23,6 +26,22 @@ public class MissionPlanifiee extends MissionEnPreparation{
 			 ArrayList<Personne> l = new ArrayList<Personne>();
 			 l.add(p);
 			 this.ListPersonneParCometence.put(Compet,l);
+		 }
+	 }
+	 
+	 //remplie la liste de personne ayant chaque compétence demandée pour la réalisation de la mission
+	 public void setEligiblePersPourMission(ArrayList<Personne> listP){
+		 
+		 for(Map.Entry<Competence,Integer> entry : this.getNbPersonneParComp().entrySet()){
+			 Competence c =entry.getKey();
+				 for(int i=0;i<listP.size();i++){
+					 //si la personne posséde la compétence demandée(actuelle) on l ajouteà la liste personne qui lui correspond
+					 Personne p = listP.get(i);
+					
+					 if(p.getListCompetences().contains(c)){
+						 this.affecterPersonne(listP.get(i), entry.getKey());
+					 }
+				  }
 		 }
 	 }
 	 

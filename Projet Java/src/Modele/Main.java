48ffc5a612b94a9af9b146928eb49ef2f221d5ed
mahachaouch <maha-chaouch@hotmail.fr>
2017-02-28 00:44:36 +0100

    adapter main aux modif de la classe competence

--- Projet Java/src/Modele/Main.java
+++ Projet Java/src/Modele/Main.java
@@ -1,51 +1,59 @@
 package Modele;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class Main {
 
-//	public static void main(String[] args) {
-//		MissionPlanifiee m = new MissionPlanifiee();
-//
-//		
-//		Personne p = new Personne("Jean","Dupont", 1, new Date("01/01/2017"));
-//		Personne p1 = new Personne("Julia","Dupont", 1, new Date("02/01/2017"));
-//		Personne p2 = new Personne("Julien","Marcel", 1, new Date("01/02/2017"));
-//		
-////		Competence c = new Competence("Marketing","M.1.");
-////		Competence c1 = new Competence("Management","M.2.");
-////		Competence c3 = new Competence("Analyse","A.3.");
-////		Competence cpt = new Competence ("Gestion","g1");
-//		
-//		
-//		p.ajouterCompetence(c1);
-//		p.ajouterCompetence(c);
-//		p1.ajouterCompetence(c3);
-//		p1.ajouterCompetence(c1);
-//		p2.ajouterCompetence(c3);
-//		
-//		
-//		m.ajouterNbPers(4, c1);
-//		m.ajouterNbPers(2, c);
-//		m.setDateDebut(new Date("02/01/2017"));
-//		//System.out.println(m.getDateDebut());
-//		
-//		
-//		
-//		m.afficherListNbPersParCompt();
-//		m.modifierNbPersonne(5, c1);
-//		m.afficherListNbPersParCompt();
-//		
-//		m.affecterPersonne(p, cpt);
-//		m.affecterPersonne(p2, c1);
-//		m.affecterPersonne(p1, c1);
-//		m.affecterPersonne(p1, cpt);
-//		m.afficherListPersParCompt();
-//		 //System.out.println(m.getListPersonneParCometence().get(c1.getCode()));
-//		System.out.println(p1.getNbMission());
-//		System.out.println(p1.getListCompetences().toString());
-//	}
+	public static void main(String[] args) {
+		MissionPlanifiee m = new MissionPlanifiee();
+		ArrayList<Personne> allPersons = new ArrayList<Personne>();
+
+		
+		Personne p = new Personne("Jean","Dupont", 1, new Date("01/01/2017"));
+		Personne p1 = new Personne("Julia","Dupont", 1, new Date("02/01/2017"));
+		Personne p2 = new Personne("Julien","Marcel", 1, new Date("01/02/2017"));
+		
+		allPersons.add(p);
+		allPersons.add(p1);
+		allPersons.add(p2);
+		
+		Competence c = new Competence("Marketing","M.1.","");
+		Competence c1 = new Competence("Management","M.2.","");
+		Competence c3 = new Competence("Analyse","A.3.","");
+		Competence cpt = new Competence ("Gestion","g1","");
+		
+		
+		p.ajouterCompetence(c1);
+		p.ajouterCompetence(c);
+		p1.ajouterCompetence(c3);
+		p1.ajouterCompetence(c1);
+		p2.ajouterCompetence(c3);
+		
+		
+		m.ajouterNbPers(4, c1);
+		m.ajouterNbPers(2, c);
+		m.setDateDebut(new Date("02/01/2017"));
+		//System.out.println(m.getDateDebut());
+		
+		
+		
+		m.afficherListNbPersParCompt();
+		m.modifierNbPersonne(5, c1);
+		m.afficherListNbPersParCompt();
+		
+		
+		
+		 //System.out.println(m.getListPersonneParCometence().get(c1.getCode()));
+		//System.out.println(p1.getNbMission());
+		//System.out.println(p1.getListCompetences().toString());
+		
+		//collecter les personnes eligibles pour une mission (par compétence demandée)
+		m.setEligiblePersPourMission(allPersons);
+		System.out.println(m.getListPersonneParCometence().toString());
+		
+	}
 
 }
